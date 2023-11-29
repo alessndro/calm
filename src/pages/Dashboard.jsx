6a822344ffc4623c27a43875d16c2d643e3031ab
Alessandro Degenkamp <alessandrodegenkamp@hotmail.com>
2023-11-29 13:55:17 +0100

    changed to retrieving <li> items, and having already <ol> in document

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -90,7 +90,7 @@ export default function Dashboard() {
         
         
               <h4 className='mb-3'>Recommendations</h4>
-              <p className='fade-in max-w-sm'>{recommendations ? recommendations : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}</p>
+              {recommendations ? <ol className='fade-in max-w-sm'>{recommendations}</ol> : <p className='fade-in max-w-sm'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p> }
               <p className='absolute bottom-2 right-5 font-black'>POWERED BY AI</p>
       
             
