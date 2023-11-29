74794838410d9460844802a25fc0dd87dd7cb9e3
Alessandro Degenkamp <alessandrodegenkamp@hotmail.com>
2023-11-29 13:52:51 +0100

    changing how ai is returned now with <li> items

--- netlify/functions/openAI/openAI.js
+++ netlify/functions/openAI/openAI.js
@@ -11,7 +11,7 @@ const handler = async (event) => {
   try {
     const response = await openai.completions.create({
       model:'text-davinci-003',
-      prompt: `You're a Welness and lifestyle Coach at a bussiness called Calm. This app is build on five elements of welness: sleep, movement, Morning Nutrition and relationships. Provide science based recommendations on the following user's score. You're providing three clear and constructive recommendations. Create an ordered list using one <ol> tag and for the recommendations <li> tags. User information: ${event.body}
+      prompt: `You're a Welness and lifestyle Coach at a bussiness called Calm. This app is build on five elements of welness: sleep, movement, Morning Nutrition and relationships. Provide science based recommendations on the following user's score. You're providing three clear and constructive recommendations. Create list items for the recommendations by using <li> tags. User information: ${event.body}
       recommendations:`,
       max_tokens: 200,
       })
