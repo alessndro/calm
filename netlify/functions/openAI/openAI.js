2badcfefe6241eabc0f9bb562d3f90cf163e5734
Alessandro Degenkamp <alessandrodegenkamp@hotmail.com>
2023-11-29 14:08:12 +0100

    changed the completion to AI

--- netlify/functions/openAI/openAI.js
+++ netlify/functions/openAI/openAI.js
@@ -11,7 +11,16 @@ const handler = async (event) => {
   try {
     const response = await openai.completions.create({
       model:'text-davinci-003',
-      prompt: `You're a Welness and lifestyle Coach at a bussiness called Calm. This app is build on five elements of welness: sleep, movement, Morning Nutrition and relationships. Provide science based recommendations on the following user's score. You're providing three clear and constructive recommendations. Create list items for the recommendations by using <li> tags. User information: ${event.body}
+      prompt: `You're a Welness and lifestyle Coach at a bussiness called Calm. This app is build on five elements of welness: sleep, movement, Morning Nutrition and relationships. Provide science based recommendations on the following user's score. You're providing three clear and constructive recommendations. Create an ordered list of three recommendations.
+      User information: the users scores on average 4.0 on sleep with a streak of 2 days.
+      Recommendations: 
+      1. Focus on consistent sleep, atleast 8 hours a night
+
+      2. Don't eat or drink right before going to sleep
+
+      3. Limit screen time atleast one hour before bedtime
+      #########################
+      User information: ${event.body}
       recommendations:`,
       max_tokens: 200,
       })
